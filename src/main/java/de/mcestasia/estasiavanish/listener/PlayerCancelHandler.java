b59e5f9a02fe2ccb5769f8fb95c62bb92172d9f5
marc.liebe <git@marc-liebe.de>
2022-04-06 14:56:48 +0200

    ✨ Protect Farmland when in Vanish
    Signed-off-by: marc.liebe <git@marc-liebe.de>

--- src/main/java/de/mcestasia/estasiavanish/listener/PlayerCancelHandler.java
+++ src/main/java/de/mcestasia/estasiavanish/listener/PlayerCancelHandler.java
@@ -1,12 +1,17 @@
 package de.mcestasia.estasiavanish.listener;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 import static de.mcestasia.estasiavanish.util.Constants.vanishList;
 
@@ -29,6 +34,19 @@ public class PlayerCancelHandler implements Listener {
         if (!(event.getEntity() instanceof Zombie) || !(event.getTarget() instanceof Player) || !(vanishList.contains((Player) event.getTarget())))
             return;
         event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void handlePlayerInteract(PlayerInteractEvent event) {
+        if (!(vanishList.contains(event.getPlayer()))) return;
+        if (event.getAction() == Action.PHYSICAL) {
+            Block block = event.getClickedBlock();
+            assert block != null;
+            if (block.getType() == Material.FARMLAND) {
+                event.setUseInteractedBlock(Event.Result.DENY);
+                event.setCancelled(true);
+            }
+        }
     }
 
 }
